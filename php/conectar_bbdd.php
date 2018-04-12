4001d4c1920d61df80f530660bca4e2538dd93b9
Zello <kristianmartinezgarcia@gmail.com>
2018-04-12 09:26:13 +0000

    Añadida conexion a bbdd

---
+++ php/conectar_bbdd.php
@@ -0,0 +1,15 @@
+<?php
+
+    $host_db = "localhost";
+    $user_db = "root";                     
+    $pass_db = "";                                  
+    $db_name = "dsi";                                  
+    $port = 3306;   
+	  
+	$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+	 
+	if ($conexion->connect_error) {
+		die("La conexion falló: " . $conexion->connect_error);
+    }
+    
+?>
