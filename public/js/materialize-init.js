775db6a36ca9f2ded3b0d7477ea5ab937db72af5
Lucía Muñoz González <alu01000949193@ull.edu.es>
2018-04-28 15:09:36 +0000

    nuevo index

--- public/js/materialize-init.js
+++ public/js/materialize-init.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
-  $('.sidenav').sidenav();
-  $('.parallax').parallax();
-  $('.slider').slider({interval:60000});
-  $('.materialboxed').materialbox();
-});
+  var elem = document.querySelector('.slider');
+  var instance = M.Slider.init(elem, {interval: 60000});
+  
+  elem = document.querySelector('.modal');
+  var instance = M.Modal.init(elem, {});
+  
+  elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.init(elem, {});
